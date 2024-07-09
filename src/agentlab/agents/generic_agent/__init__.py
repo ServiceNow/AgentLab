e1f091f966476829fbe936083a354c6714091998
Thibault Le Sellier de Chezelles <thibault.de.chezelles@gmail.com>
2024-07-09 14:49:15 -0400

    adding main imports

---
+++ src/agentlab/agents/generic_agent/__init__.py
@@ -0,0 +1,38 @@
+from .agent_configs import AGENT_3_5, AGENT_4o, AGENT_4o_VISION, AGENT_70B, AGENT_8B
+
+from .generic_agent import GenericAgent
+
+from .exp_configs import (
+    generic_agent_test,
+    tgi_toolkit_test,
+    generic_agent_eval_llm,
+    random_search,
+    progression_study,
+    final_run,
+    ablation_study,
+    ablation_study_GPT_3_5,
+    ablation_study_OSS,
+    ablation_study_GPT_4,
+    demo_maker,
+)
+
+
+__all__ = [
+    "AGENT_3_5",
+    "AGENT_4o",
+    "AGENT_4o_VISION",
+    "AGENT_70B",
+    "AGENT_8B",
+    "GenericAgent",
+    "generic_agent_test",
+    "tgi_toolkit_test",
+    "generic_agent_eval_llm",
+    "random_search",
+    "progression_study",
+    "final_run",
+    "ablation_study",
+    "ablation_study_GPT_3_5",
+    "ablation_study_OSS",
+    "ablation_study_GPT_4",
+    "demo_maker",
+]
