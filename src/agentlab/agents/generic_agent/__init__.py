74031588dfb95bce26485dc0b94f72664f8cf82a
ThibaultLSDC <thibault.de.chezelles@gmail.com>
2024-07-15 08:02:13 -0400

    fixing tests

--- src/agentlab/agents/generic_agent/__init__.py
+++ src/agentlab/agents/generic_agent/__init__.py
@@ -3,14 +3,10 @@ from .agent_configs import AGENT_3_5, AGENT_4o, AGENT_4o_VISION, AGENT_70B, AGEN
 from .exp_configs import (
     generic_agent_test,
     tgi_toolkit_test,
-    generic_agent_eval_llm,
     random_search,
     progression_study,
     final_run,
     ablation_study,
-    ablation_study_GPT_3_5,
-    ablation_study_OSS,
-    ablation_study_GPT_4,
     demo_maker,
 )
 
@@ -23,13 +19,9 @@ __all__ = [
     "AGENT_8B",
     "generic_agent_test",
     "tgi_toolkit_test",
-    "generic_agent_eval_llm",
     "random_search",
     "progression_study",
     "final_run",
     "ablation_study",
-    "ablation_study_GPT_3_5",
-    "ablation_study_OSS",
-    "ablation_study_GPT_4",
     "demo_maker",
 ]
--- tests/experiments/test_exp_configs.py
+++ tests/experiments/test_exp_configs.py
@@ -7,7 +7,6 @@ def test_all_configs():
     exp_name_list = [
         "agentlab.agents.generic_agent.generic_agent_test",
         "agentlab.agents.generic_agent.tgi_toolkit_test",
-        "agentlab.agents.generic_agent.generic_agent_eval_llm",
         "agentlab.agents.generic_agent.progression_study",
         "agentlab.agents.generic_agent.ablation_study",
         "agentlab.agents.generic_agent.demo_maker",
@@ -20,3 +19,7 @@ def test_all_configs():
         assert isinstance(exp_args_list, list)
         assert len(exp_args_list) > 0
         assert all(isinstance(exp_args, ExpArgs) for exp_args in exp_args_list)
+
+
+if __name__ == "__main__":
+    test_all_configs()
