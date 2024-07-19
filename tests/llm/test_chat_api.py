3b30e631f37fe7b10ae38bc0c89243b434953af8
Thibault Le Sellier de Chezelles <thibault.de.chezelles@gmail.com>
2024-07-19 13:54:19 -0400

    making separate ModelArgs for each APIs

--- tests/llm/test_chat_api.py
+++ tests/llm/test_chat_api.py
@@ -1,19 +1,25 @@
+import os
+
 import pytest
 from langchain.schema import HumanMessage, SystemMessage
 
-from agentlab.llm.chat_api import APIModelArgs
+from agentlab.llm.chat_api import AzureModelArgs, HuggingFaceModelArgs, OpenAIModelArgs
 from agentlab.llm.llm_utils import download_and_save_model
 from agentlab.llm.prompt_templates import STARCHAT_PROMPT_TEMPLATE
 
 # TODO(optimass): figure out a good model for all tests
 
 
-@pytest.mark.pricy
-@pytest.mark.skip(reason="Skipping atm for lack of better marking")
-def test_api_model_args_hf():
-    model_name = "huggingface/HuggingFaceH4/starchat-beta"
+if "AGENTLAB_LOCAL_TEST" in os.environ:
+    skip_tests = os.environ["AGENTLAB_LOCAL_TEST"] != "1"
 
-    model_args = APIModelArgs(
+
+@pytest.mark.pricy
+@pytest.mark.skipif(skip_tests, reason="Skipping on remote as HF token have limited usage")
+def test_api_model_args_hf():
+    model_name = "HuggingFaceH4/starchat-beta"
+
+    model_args = HuggingFaceModelArgs(
         model_name=model_name,
         max_total_tokens=8192,
         max_input_tokens=8192 - 512,
@@ -32,10 +38,32 @@ def test_api_model_args_hf():
 
 
 @pytest.mark.pricy
-@pytest.mark.skip(reason="Skipping atm for lack of better marking")
+@pytest.mark.skipif(skip_tests, reason="Skipping on remote as Azure is pricy")
 def test_api_model_args_azure():
-    model_args = APIModelArgs(
-        model_name="azure/gpt-35-turbo/gpt-35-turbo",
+    model_args = AzureModelArgs(
+        model_name="gpt-35-turbo",
+        deployment_name="gpt-35-turbo",
+        max_total_tokens=8192,
+        max_input_tokens=8192 - 512,
+        max_new_tokens=512,
+        temperature=1e-1,
+    )
+    model = model_args.make_model()
+
+    messages = [
+        SystemMessage(content="You are an helpful virtual assistant"),
+        HumanMessage(content="Give the third prime number"),
+    ]
+    answer = model.invoke(messages)
+
+    assert "5" in answer.content
+
+
+@pytest.mark.pricy
+@pytest.mark.skip(reason="Skipping atm for lack of better marking")
+def test_api_model_args_openai():
+    model_args = OpenAIModelArgs(
+        model_name="gpt-3.5-turbo-0125",
         max_total_tokens=8192,
         max_input_tokens=8192 - 512,
         max_new_tokens=512,
