b8d6a8e0e34515089efbfb95dea2dccabe9311ea
Thibault Le Sellier de Chezelles <thibault.de.chezelles@gmail.com>
2024-07-19 13:54:19 -0400

    making separate ModelArgs for each APIs

--- src/agentlab/llm/chat_api.py
+++ src/agentlab/llm/chat_api.py
@@ -7,7 +7,7 @@ from typing import TYPE_CHECKING
 from langchain.schema import AIMessage
 from langchain_openai import AzureChatOpenAI, ChatOpenAI
 
-from agentlab.llm.langchain_utils import HuggingFaceChatModel, RekaChatModel
+from agentlab.llm.langchain_utils import HuggingFaceAPIChatModel, HuggingFaceURLChatModel
 
 if TYPE_CHECKING:
     from langchain_core.language_models.chat_models import BaseChatModel
@@ -88,57 +88,11 @@ class BaseModelArgs(ABC):
 
 
 @dataclass
-class APIModelArgs(BaseModelArgs):
-    """Serializable object for instantiating a generic chat model with an API."""
-
-    def make_model(self):
-        base_name = self.model_name.split("/")[0]
-        if base_name == "openai":
-            model_name = self.model_name.split("/")[-1]
-
-            return ChatOpenAI(
-                model_name=model_name,
-                temperature=self.temperature,
-                max_tokens=self.max_new_tokens,
-            )
-        elif base_name == "azure":
-            model_name = self.model_name.split("/")[1]
-            deployment_name = self.model_name.split("/")[2]
-            return AzureChatOpenAI(
-                model_name=model_name,
-                temperature=self.temperature,
-                max_tokens=self.max_new_tokens,
-                deployment_name=deployment_name,
-            )
-        elif base_name == "reka":
-            model_name = self.model_name.split("/")[-1]
-            return RekaChatModel(
-                model_name=model_name,
-            )
-        elif base_name == "huggingface":
-            return HuggingFaceChatModel(
-                model_name=self.model_name.split("/", 1)[1],
-                hf_hosted=True,
-                temperature=self.temperature,
-                max_new_tokens=self.max_new_tokens,
-                max_total_tokens=self.max_total_tokens,
-                max_input_tokens=self.max_input_tokens,
-                model_url=None,
-                n_retry_server=4,
-            )
-        else:
-            raise ValueError(f"Backend {base_name} is not supported")
-
-
-@dataclass
-class OpenAiModelArgs(BaseModelArgs):
+class OpenAIModelArgs(BaseModelArgs):
     """Serializable object for instantiating a generic chat model with an OpenAI
     model."""
 
     def make_model(self):
-        if self.base_model_name != "openai":
-            raise ValueError(f"Model {self.model_name} is not an OpenAI model")
-
         return ChatOpenAI(
             model_name=self.short_model_name,
             temperature=self.temperature,
@@ -146,19 +100,31 @@ class OpenAiModelArgs(BaseModelArgs):
         )
 
 
+@dataclass
 class HuggingFaceModelArgs(BaseModelArgs):
     """Serializable object for instantiating a generic chat model with a HuggingFace model."""
 
     def make_model(self):
-        return HuggingFaceChatModel(
+        return HuggingFaceAPIChatModel(
             model_name=self.model_name,
-            hf_hosted=True,
             temperature=self.temperature,
             max_new_tokens=self.max_new_tokens,
-            max_total_tokens=self.max_total_tokens,
-            max_input_tokens=self.max_input_tokens,
-            model_url=None,
             n_retry_server=4,
+        )
+
+
+@dataclass
+class AzureModelArgs(BaseModelArgs):
+    """Serializable object for instantiating a generic chat model with an Azure model."""
+
+    deployment_name: str = None
+
+    def make_model(self):
+        return AzureChatOpenAI(
+            model_name=self.model_name,
+            temperature=self.temperature,
+            max_tokens=self.max_new_tokens,
+            deployment_name=self.deployment_name,
         )
 
 
@@ -178,25 +144,13 @@ class SelfHostedModelArgs(BaseModelArgs):
                 self.model_url = os.environ["AGENTLAB_MODEL_URL"]
             if self.token is None:
                 self.token = os.environ["AGENTLAB_MODEL_TOKEN"]
-            # client = InferenceClient(model=self.model_url, token=self.token)
-            # llm = HuggingFaceEndpoint(
-            #     name=self.model_name,
-            #     endpoint_url=self.model_url,
-            #     max_new_tokens=self.max_new_tokens,
-            #     temperature=self.temperature,
-            #     client=client,
-            # )
 
-            # return ChatHuggingFace(model_id=self.model_name, llm=llm)
-
-            return HuggingFaceChatModel(
+            return HuggingFaceURLChatModel(
                 model_name=self.model_name,
-                hf_hosted=False,
+                model_url=self.model_url,
+                token=self.token,
                 temperature=self.temperature,
                 max_new_tokens=self.max_new_tokens,
-                max_total_tokens=self.max_total_tokens,
-                max_input_tokens=self.max_input_tokens,
-                model_url=self.model_url,
                 n_retry_server=self.n_retry_server,
             )
         else:
