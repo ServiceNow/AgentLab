9b045de6ce010845f6287f9ad8b80cd3b0505deb
recursix <alex.lacoste.shmu@gmail.com>
2024-08-20 11:30:57 -0400

    update ui assistant and make unit test

--- src/agentlab/ui_assistant.py
+++ src/agentlab/ui_assistant.py
@@ -1,58 +1,56 @@
-from browsergym.experiments.loop import ExpArgs
+import argparse
 
-from agentlab.agents.dynamic_prompting import Flags
-from agentlab.agents.generic_agent.generic_agent import GenericAgentArgs
+from browsergym.experiments.loop import AbstractAgentArgs, EnvArgs, ExpArgs
+
 from agentlab.experiments.exp_utils import RESULTS_DIR
-from agentlab.llm.chat_api import OpenAIModelArgs
+from agentlab.experiments.launch_exp import import_object
 
-start_url = "https://www.google.com"
 
-exp_args = ExpArgs(
-    agent_args=GenericAgentArgs(
-        chat_model_args=OpenAIModelArgs(
-            model_name="openai/gpt-4-vision-preview",
-            max_total_tokens=128_000,
-            max_input_tokens=40_000,  # make sure we don't bust budget
-            max_new_tokens=4000,  # I think this model has very small default value if we don't set max_new_tokens
+def make_exp_args(agent_args: AbstractAgentArgs, start_url="https://www.google.com"):
+
+    try:
+        agent_args.flags.action.demo_mode = "default"
+    except AttributeError:
+        pass
+
+    exp_args = ExpArgs(
+        agent_args=agent_args,
+        env_args=EnvArgs(
+            max_steps=1000,
+            task_seed=None,
+            task_name="openended",
+            task_kwargs={
+                "start_url": start_url,
+            },
+            headless=False,
+            record_video=True,
+            wait_for_user_message=True,
+            viewport={"width": 1500, "height": 1280},
+            slow_mo=1000,
         ),
-        flags=Flags(
-            use_plan=False,
-            use_html=False,
-            use_ax_tree=True,
-            use_focused_element=True,
-            use_criticise=False,
-            use_thinking=True,
-            use_error_logs=True,
-            use_memory=False,
-            use_history=True,
-            use_diff=False,
-            use_past_error_logs=True,
-            use_action_history=True,
-            use_think_history=True,
-            multi_actions=True,
-            use_hints=True,
-            use_abstract_example=True,
-            use_concrete_example=True,
-            use_screenshot=True,
-            enable_chat=True,
-            demo_mode="default",
-            action_space="bid+coord+nav",
-            extract_coords="center",
-        ),
-    ),
-    max_steps=100,
-    task_seed=None,
-    task_name="openended",
-    task_kwargs={
-        "start_url": start_url,
-    },
-    enable_debug=True,
-    headless=False,
-    record_video=True,
-    wait_for_user_message=True,
-    viewport={"width": 1500, "height": 1280},
-    slow_mo=1000,
-)
+    )
 
-exp_args.prepare(RESULTS_DIR / "ui_assistant_logs")
-exp_args.run()
+    return exp_args
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--agent_config",
+        type=str,
+        default="agentlab.agents.generic_agent.AGENT_4o",
+        help="Python path to the agent config",
+    )
+    parser.add_argument(
+        "--start_url",
+        type=str,
+        default="https://www.google.com",
+        help="The start page of the agent",
+    )
+
+    args, unknown = parser.parse_known_args()
+    agent_args = import_object(args.agent_config)
+    exp_args = make_exp_args(agent_args, args.start_url)
+    exp_args.prepare(RESULTS_DIR / "ui_assistant_logs")
+    exp_args.run()
