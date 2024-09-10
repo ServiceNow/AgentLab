b971a31f8b12cff2cc929931523af81c2438bdb5
Thibault LSDC <78021491+ThibaultLSDC@users.noreply.github.com>
2024-09-10 21:24:43 +0200

    adding 4o-mini config to __init__.py (#22)

--- src/agentlab/agents/generic_agent/__init__.py
+++ src/agentlab/agents/generic_agent/__init__.py
@@ -2,15 +2,16 @@ from .agent_configs import (
     AGENT_3_5,
     AGENT_8B,
     AGENT_70B,
-    AGENT_4o,
-    AGENT_4o_VISION,
     RANDOM_SEARCH_AGENT,
+    AGENT_4o,
+    AGENT_4o_MINI,
+    AGENT_4o_VISION,
 )
-
 
 __all__ = [
     "AGENT_3_5",
     "AGENT_4o",
+    "AGENT_4o_MINI",
     "AGENT_4o_VISION",
     "AGENT_70B",
     "AGENT_8B",
