0b67fc2b544b9f0213fbe67d3ec5378be5cf39c1
ThibaultLSDC <thibault.de.chezelles@gmail.com>
2024-10-09 09:52:08 -0400

    fixing main test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,7 @@
-from pathlib import Path
 import subprocess
+import sys
+from pathlib import Path
+
 import pytest
 
 
@@ -7,16 +9,18 @@ import pytest
 def test_main_script_execution():
     # this should trigger agent_4o_mini on miniwob_tiny_test unless this was
     # reconfigured differently.
-    script_path = Path(__file__).parent.parent / "main.py"
+    root_path = Path(__file__).parent.parent
+
+    sys.path.insert(0, str(root_path))
 
     # just make sure it's in the right state
-    main = __import__(script_path.stem)
+    main = __import__("main")
     assert main.benchmark == "miniwob_tiny_test"
     assert main.reproducibility_mode == False
     assert main.relaunch == False
     assert main.n_jobs <= 10
 
-    result = subprocess.run(["python", script_path], capture_output=True, text=True, timeout=5 * 60)
+    result = subprocess.run(["python", "main.py"], capture_output=True, text=True, timeout=5 * 60)
     assert result.returncode == 0
 
 
