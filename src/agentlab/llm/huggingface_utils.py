417609be3375c994150ce10bbe638c688be09bed
ThibaultLSDC <thibault.de.chezelles@gmail.com>
2024-10-15 15:18:27 -0400

    changing chat_api structure

--- src/agentlab/llm/huggingface_utils.py
+++ src/agentlab/llm/huggingface_utils.py
@@ -1,17 +1,15 @@
 import logging
-import os
 import time
-from functools import partial
 from typing import Any, List, Optional
 
-from huggingface_hub import InferenceClient
 from pydantic import Field
 from transformers import AutoTokenizer, GPT2TokenizerFast
 
+from agentlab.llm.base_api import AbstractChatModel
 from agentlab.llm.prompt_templates import PromptTemplate, get_prompt_template
 
 
-class HFBaseChatModel:  # we need this to be a subclass of AbstractChatModel
+class HFBaseChatModel(AbstractChatModel):
     """
     Custom LLM Chatbot that can interface with HuggingFace models.
 
@@ -93,98 +91,6 @@ class HFBaseChatModel:  # we need this to be a subclass of AbstractChatModel
 
     def _llm_type(self):
         return "huggingface"
-
-    def get_stats(self):
-        return {}
-
-
-class HuggingFaceURLChatModel(HFBaseChatModel):
-    def __init__(
-        self,
-        model_name: str,
-        model_url: str,
-        token: Optional[str] = None,
-        temperature: Optional[int] = 1e-1,
-        max_new_tokens: Optional[int] = 512,
-        n_retry_server: Optional[int] = 4,
-    ):
-        super().__init__(model_name, n_retry_server)
-        if temperature < 1e-3:
-            logging.warning("Models might behave weirdly when temperature is too low.")
-
-        if token is None:
-            token = os.environ["TGI_TOKEN"]
-
-        client = InferenceClient(model=model_url, token=token)
-        self.llm = partial(
-            client.text_generation, temperature=temperature, max_new_tokens=max_new_tokens
-        )
-
-
-# def _convert_messages_to_dict(messages, column_remap={}):
-#     """
-#     Converts a list of message objects into a list of dictionaries, categorizing each message by its role.
-
-#     Each message is expected to be an instance of one of the following types: SystemMessage, HumanMessage, AIMessage.
-#     The function maps each message to its corresponding role ('system', 'user', 'assistant') and formats it into a dictionary.
-
-#     Args:
-#         messages (list): A list of message objects.
-#         column_remap (dict): A dictionary that maps the column names to the desired output format.
-
-#     Returns:
-#         list: A list of dictionaries where each dictionary represents a message and contains 'role' and 'content' keys.
-
-#     Raises:
-#         ValueError: If an unsupported message type is encountered.
-
-#     Example:
-#         >>> messages = [SystemMessage("System initializing..."), HumanMessage("Hello!"), AIMessage("How can I assist?")]
-#         >>> _convert_messages_to_dict(messages)
-#         [
-#             {"role": "system", "content": "System initializing..."},
-#             {"role": "user", "content": "Hello!"},
-#             {"role": "assistant", "content": "How can I assist?"}
-#         ]
-#     """
-
-#     human_key = column_remap.get("HumanMessage", "user")
-#     ai_message_key = column_remap.get("AIMessage", "assistant")
-#     role_key = column_remap.get("role", "role")
-#     text_key = column_remap.get("text", "content")
-#     image_key = column_remap.get("image", "media_url")
-
-#     # Mapping of message types to roles
-#     message_type_to_role = {
-#         SystemMessage: "system",
-#         HumanMessage: human_key,
-#         AIMessage: ai_message_key,
-#     }
-
-#     def convert_format_vision(message_content, role, text_key, image_key):
-#         result = {}
-#         result["type"] = role
-#         for item in message_content:
-#             if item["type"] == "text":
-#                 result[text_key] = item["text"]
-#             elif item["type"] == "image_url":
-#                 result[image_key] = item["image_url"]
-#         return result
-
-#     chat = []
-#     for message in messages:
-#         message_role = message_type_to_role.get(type(message))
-#         if message_role:
-#             if isinstance(message.content, str):
-#                 chat.append({role_key: message_role, text_key: message.content})
-#             else:
-#                 chat.append(
-#                     convert_format_vision(message.content, message_role, text_key, image_key)
-#                 )
-#         else:
-#             raise ValueError(f"Message type {type(message)} not supported")
-
-#     return chat
 
 
 def _prepend_system_to_first_user(messages, column_remap={}):
