8b10f1861b21f10ca37192dfbda8a04c59c56b06
Thibault LSDC <78021491+ThibaultLSDC@users.noreply.github.com>
2024-10-16 16:15:48 -0400

    Patching minor stuff (#69)
    * fixing sample_std for single experience
    
    * making gradio shared server non default
    
    * missing requirement for xray

--- src/agentlab/analyze/agent_xray.py
+++ src/agentlab/analyze/agent_xray.py
@@ -482,7 +482,8 @@ clicking the refresh button.
         tabs.select(tab_select)
 
     demo.queue()
-    demo.launch(server_port=int(os.getenv("AGENTXRAY_APP_PORT", 7899)), share=True)
+    do_share = os.getenv("AGENTXRAY_SHARE_SERVER", "false").lower() == "true"
+    demo.launch(server_port=int(os.getenv("AGENTXRAY_APP_PORT", 7899)), share=do_share)
 
 
 def tab_select(evt: gr.SelectData):
