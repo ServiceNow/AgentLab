057b7d4a201cc1cd1ebd7bc884f6a91e104c479d
ThibaultLSDC <thibault.de.chezelles@gmail.com>
2024-10-17 10:50:38 -0400

    Adding inspect_result.ipynb to reprod white list

--- src/agentlab/experiments/reproducibility_util.py
+++ src/agentlab/experiments/reproducibility_util.py
@@ -173,6 +173,7 @@ def get_reproducibility_info(
         "*/reproducibility_script.py",
         "*reproducibility_journal.csv",
         "*main.py",
+        "*inspect_results.ipynb",
     ),
     ignore_changes=False,
 ):
