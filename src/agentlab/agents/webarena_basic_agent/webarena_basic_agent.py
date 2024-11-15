6442ba1d5b32b07789c1bc93db03ffe86404bbf3
Maxime Gasse <maxime.gasse@gmail.com>
2024-11-15 17:47:20 -0500

    webarena agent replica update

--- src/agentlab/agents/webarena_basic_agent/webarena_basic_agent.py
+++ src/agentlab/agents/webarena_basic_agent/webarena_basic_agent.py
@@ -1,13 +1,10 @@
-import logging
-import os
-import re
+from copy import deepcopy
 from dataclasses import asdict, dataclass
 from typing import TYPE_CHECKING, Any
 
 from browsergym.core.action.highlevel import HighLevelActionSet
-from browsergym.experiments.agent import Agent, AgentInfo
-from browsergym.experiments.loop import EnvArgs, ExpArgs
-from langchain.schema import AIMessage, HumanMessage, SystemMessage
+from browsergym.experiments import Agent, AgentInfo
+from browsergym.experiments.benchmark import Benchmark, HighLevelActionSetArgs
 
 from agentlab.agents.agent_args import AgentArgs
 from agentlab.llm.chat_api import make_system_message, make_user_message
@@ -22,8 +19,9 @@ if TYPE_CHECKING:
 @dataclass
 class WebArenaBasicAgentArgs(AgentArgs):
     agent_name: str = "WebArenaBasicAgent"
-    temperature: float = 0.0
-    chat_model_args: "BaseModelArgs" = None
+    temperature: float
+    chat_model_args: "BaseModelArgs"
+    action_set_args: HighLevelActionSetArgs
 
     def __post_init__(self):
         self.agent_name = f"WebArenaBasicAgent-{self.chat_model_args.model_name}".replace("/", "_")
@@ -32,13 +30,85 @@ class WebArenaBasicAgentArgs(AgentArgs):
         return WebArenaBasicAgent(
             temperature=self.temperature,
             chat_model_args=self.chat_model_args,
+            action_set=HighLevelActionSetArgs.make_action_set(),
         )
+
+    def set_benchmark(self, benchmark: Benchmark, demo_mode: bool):
+        self.action_set_args = deepcopy(benchmark.high_level_action_set_args)
+
+    def set_reproducibility_mode(self):
+        self.temperature = 0.0
 
     def prepare(self):
         return self.chat_model_args.prepare_server()
 
     def close(self):
         return self.chat_model_args.close_server()
+
+
+# Best-attempt reproduction of original prompts from
+# https://github.com/web-arena-x/webarena/blob/e31c190c9b43f63e5724322b847e00249300df40/agent/prompts/raw/p_cot_id_actree_2s_no_na.py#L1
+webarena_intro_template = """\
+You are an autonomous intelligent agent tasked with navigating a web browser. You will be given web-based tasks. These tasks will be accomplished through the use of specific actions you can issue.
+
+Here's the information you'll have:
+The user's objective: This is the task you're trying to complete.
+The current web page's accessibility tree: This is a simplified representation of the webpage, providing key information.
+The current web page's URL: This is the page you're currently navigating.
+The open tabs: These are the tabs you have open.
+The previous action: This is the action you just performed. It may be helpful to track your progress.
+
+{action_space_description}
+
+To be successful, it is very important to follow the following rules:
+1. You should only issue an action that is valid given the current observation
+2. You should only issue one action at a time.
+3. You should follow the examples to reason step by step and then issue the next action.
+4. Generate the action in the correct format. Start with a "In summary, the next action I will perform is" phrase, followed by action inside ``````. For example, "In summary, the next action I will perform is ```click("1234")```".
+"""
+
+webarena_examples = [
+    """\
+OBSERVATION:
+[1744] link 'HP CB782A#ABA 640 Inkjet Fax Machine (Renewed)'
+		[1749] StaticText '$279.49'
+		[1757] button 'Add to Cart'
+		[1760] button 'Add to Wish List'
+		[1761] button 'Add to Compare'
+URL: http://onestopmarket.com/office-products/office-electronics.html
+OBJECTIVE: What is the price of HP Inkjet Fax Machine
+PREVIOUS ACTION: None
+ACTION: Let's think step-by-step. This page list the information of HP Inkjet Fax Machine, which is the product identified in the objective. Its price is $279.49. I think I have achieved the objective. I will issue the stop action with the answer. In summary, the next action I will perform is ```stop [$279.49]```
+""",
+    """\
+OBSERVATION:
+[164] textbox 'Search' focused: True required: False
+[171] button 'Go'
+[174] link 'Find directions between two points'
+[212] heading 'Search Results'
+[216] button 'Close'
+URL: http://openstreetmap.org
+OBJECTIVE: Show me the restaurants near CMU
+PREVIOUS ACTION: None
+ACTION: Let's think step-by-step. This page has a search box whose ID is [164]. According to the nominatim rule of openstreetmap, I can search for the restaurants near a location by \"restaurants near\". I can submit my typing by pressing the Enter afterwards. In summary, the next action I will perform is ```type [164] [restaurants near CMU] [1]```
+""",
+]
+
+webarena_template = """\
+OBSERVATION:
+{observation}
+URL: {url}
+OBJECTIVE: {objective}
+PREVIOUS ACTION: {previous_action}"""
+
+
+def parser(response: str) -> tuple[dict, bool, str]:
+    blocks = extract_code_blocks(response)
+    if len(blocks) == 0:
+        raise ParseError("No code block found in the response")
+    action = blocks[0][1]
+    thought = response
+    return {"action": action, "think": thought}
 
 
 class WebArenaBasicAgent(Agent):
@@ -48,65 +118,67 @@ class WebArenaBasicAgent(Agent):
         self.chat_model_args = chat_model_args
 
         self.action_set = HighLevelActionSet(["chat", "bid"], strict=False, multiaction=False)
-        self.prev_actions = []
+        self.action_history = ["None"]
 
     @cost_tracker_decorator
     def get_action(self, obs: Any) -> tuple[str, dict]:
-        system_prompt = f"""\
-# Instructions
-Review the current state of the page and all other information to find the best
-possible next action to accomplish your goal. Your answer will be interpreted
-and executed by a program, make sure to follow the formatting instructions.
+        """
+        Replica of WebArena agent
+        https://github.com/web-arena-x/webarena/blob/e31c190c9b43f63e5724322b847e00249300df40/agent/prompts/prompt_constructor.py#L38
+        """
+        messages = []
 
-# Goal:
-{obs["goal"]}
+        # intro
+        messages.append(
+            make_system_message(
+                webarena_intro_template.format(
+                    action_space_description=self.action_set.describe(
+                        with_long_description=False, with_examples=False
+                    )
+                )
+            )
+        )
 
-# Action Space
-{self.action_set.describe(with_long_description=False, with_examples=True)}
+        # examples
+        for i, example in enumerate(webarena_examples):
+            messages.append(
+                make_system_message(
+                    f"""\
+Example {i + 1}/{len(webarena_examples)}:
+
+{example}
 """
+                )
+            )
+
+        # current observation
+        cur_tabs_txt = " | ".join(
+            f"Tab {i}{' (current)' if i == obs['active_page_index'][0] else ''}: {title}"
+            for i, title in enumerate(obs["open_pages_titles"])
+        )
         cur_axtree_txt = obs["axtree_txt"]
-        prev_action_str = "\n".join(self.prev_actions)
 
-        prompt = f"""\
-# Current Accessibility Tree:
+        messages.append(
+            make_user_message(
+                webarena_template.format(
+                    observation=f"""\
+{cur_tabs_txt}
+
 {cur_axtree_txt}
-
-# Previous Actions
-{prev_action_str}
-
-Here is an example with chain of thought of a valid action when clicking on a button:
-"
-In order to accomplish my goal I need to click on the button with bid 12
-```
-click("12")
-```
-"
-
-Here is another example with chain of thought of a valid action when providing a concise answer to user:
-"
-In order to accomplish my goal I need to send the information asked back to the user. This page list the information of HP Inkjet Fax Machine, which is the product identified in the objective. Its price is $279.49. I will send a message back to user with the answer.
-```
-send_msg_to_user("$279.49")
-```
-"
-""".strip()
-
-        messages = [make_system_message(system_prompt), make_user_message(prompt)]
-
-        def parser(response: str) -> tuple[dict, bool, str]:
-            blocks = extract_code_blocks(response)
-            if len(blocks) == 0:
-                raise ParseError("No code block found in the response")
-            action = blocks[0][1]
-            thought = response
-            return {"action": action, "think": thought}
+""",
+                    url=obs["url"],
+                    objective=obs["goal"],  # TODO convert goal_object to text (images?)
+                    previous_action=self.action_history[-1],
+                )
+            )
+        )
 
         ans_dict = retry(self.chat, messages, n_retry=3, parser=parser)
 
         action = ans_dict.get("action", None)
         thought = ans_dict.get("think", None)
 
-        self.prev_actions.append(action)
+        self.action_history.append(action)
 
         return (
             action,
@@ -114,21 +186,13 @@ send_msg_to_user("$279.49")
                 think=thought,
                 chat_messages=messages,
                 # put any stats that you care about as long as it is a number or a dict of numbers
-                stats={"prompt_length": len(prompt), "response_length": len(thought)},
-                markdown_page="Add any txt information here, including base 64 images, to display in xray",
+                stats={
+                    "prompt_length": sum(len(message.content) for message in messages),
+                    "response_length": len(thought),
+                },
                 extra_info={"chat_model_args": asdict(self.chat_model_args)},
             ),
         )
-
-
-env_args = EnvArgs(
-    task_name="miniwob.click-button",
-    task_seed=0,
-    max_steps=10,
-    headless=True,
-)
-
-chat_model_args = CHAT_MODEL_ARGS_DICT["openai/gpt-4o-mini-2024-07-18"]
 
 
 AGENT_4o_MINI = WebArenaBasicAgentArgs(
@@ -145,41 +209,3 @@ AGENT_SONNET = WebArenaBasicAgentArgs(
     temperature=0.1,
     chat_model_args=CHAT_MODEL_ARGS_DICT["openrouter/anthropic/claude-3.5-sonnet:beta"],
 )
-
-
-exp_args = [
-    ExpArgs(
-        agent_args=WebArenaBasicAgentArgs(
-            temperature=0.1,
-            chat_model_args=chat_model_args,
-        ),
-        env_args=env_args,
-        logging_level=logging.INFO,
-    ),
-    ExpArgs(
-        agent_args=WebArenaBasicAgentArgs(
-            temperature=0.0,
-            chat_model_args=chat_model_args,
-        ),
-        env_args=env_args,
-        logging_level=logging.INFO,
-    ),
-]
-
-
-def experiment_config():
-    return exp_args
-
-
-if __name__ == "__main__":
-    from agentlab.experiments.exp_utils import RESULTS_DIR
-
-    exp_dir = os.path.join(RESULTS_DIR, "webarena_basic_agent")
-
-    for exp_arg in exp_args:
-        exp_arg.agent_args.prepare()
-        exp_arg.prepare(exp_dir)
-
-    for exp_arg in exp_args:
-        exp_arg.run()
-        exp_arg.agent_args.close()
