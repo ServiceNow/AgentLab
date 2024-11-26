dcd9e1514b92d961247ef86d17978c6821b8795b
ThibaultLSDC <thibault.de.chezelles@gmail.com>
2024-11-26 11:43:25 -0500

    updates

--- docs/conf.py
+++ docs/conf.py
@@ -6,25 +6,24 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'AgentLab'
-copyright = '2024, ServiceNow'
-author = 'ServiceNow'
-release = '0.3.1'
+project = "AgentLab"
+copyright = "2024, ServiceNow"
+author = "ServiceNow"
+release = "0.3.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
+    "sphinx.ext.autodoc",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx-rtd-theme"
+html_static_path = ["_static"]
