840e34265fe78e9336490317beb8f692946a31cf
Aziz Diop <mouhamed-abdoul-aziz.diop@polymtl.ca>
2024-11-29 14:29:41 -0500

    applying black format

--- myExp.py
+++ myExp.py
@@ -2,9 +2,7 @@ from agentlab.agents.generic_agent import AGENT_4o_MINI
 from agentlab.experiments.study import make_study
 
 study = make_study(
-    benchmark="miniwob_tiny_test",  
-    agent_args=[AGENT_4o_MINI],
-    comment="Test avec MiniWoB"
+    benchmark="miniwob_tiny_test", agent_args=[AGENT_4o_MINI], comment="Test avec MiniWoB"
 )
 
-study.run(n_jobs=1) 
+study.run(n_jobs=1)
