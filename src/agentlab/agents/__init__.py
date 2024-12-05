d503c1a27c93247de871e7dc6775f2c576c1d5bd
ThibaultLSDC <thibault.de.chezelles@gmail.com>
2024-12-05 10:52:37 -0500

    adding some documentation

--- src/agentlab/agents/__init__.py
+++ src/agentlab/agents/__init__.py
@@ -1,4 +1,13 @@
-"""TEST
+"""
+AgentLab's agent implementations
 
-Stuff written here
+This module contains the agent implementations for AgentLab. With currently:
+
+- GenericAgent: Our baseline agent for evaluation
+
+- MostBasicAgent: A basic agent for learning our framework
+
+- TapeAgent: An agent that uses the Tape data structure to perform actions
+
+- VisualWebArenaAgent: An implentation of the agent used in WebArena and VisualWebArena
 """
