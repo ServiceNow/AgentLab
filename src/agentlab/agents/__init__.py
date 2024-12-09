7ae6b82c8b8c50a395c51a3afb42b8fa6fa9d97b
Thibault LSDC <78021491+ThibaultLSDC@users.noreply.github.com>
2024-12-09 15:05:06 -0500

    automated readthedocs (#177)
    * adding readthedocs stuff
    
    * adding .readthedocs.yml
    
    * ReadTheDoc base template
    
    * fixing unused dep
    
    * fancier theme options
    
    * adding some documentation
    
    * adding some init files
    
    * adding workflow
    
    * fixing rtd git workflow
    
    * testing some more stuff
    
    * triggers only on non dev tags
    
    * changing workflows
    
    * Explicit tag push
    
    ---------
    
    Co-authored-by: Maxime Gasse <maxime.gasse@gmail.com>

---
+++ src/agentlab/agents/__init__.py
@@ -0,0 +1,13 @@
+"""
+AgentLab's pre-implemented agents.
+
+This module contains the agent implementations for AgentLab. With currently:
+
+- GenericAgent: Our baseline agent for evaluation
+
+- MostBasicAgent: A basic agent for learning our framework
+
+- TapeAgent: An agent that uses the Tape data structure to perform actions
+
+- VisualWebArenaAgent: An implentation of the agent used in WebArena and VisualWebArena
+"""
--- src/agentlab/agents/generic_agent/__init__.py
+++ src/agentlab/agents/generic_agent/__init__.py
@@ -1,9 +1,17 @@
+"""
+Baseline agent for all ServiceNow papers
+
+This module contains the GenericAgent class, which is the baseline agent for all ServiceNow papers. \
+It is a simple agent that can be ran OOB on all BrowserGym environments. It is also shipped with \
+a few configurations that can be used to run it on different environments.
+"""
+
 from .agent_configs import (
     AGENT_3_5,
     AGENT_8B,
+    AGENT_CUSTOM,
     AGENT_LLAMA3_70B,
     AGENT_LLAMA31_70B,
-    AGENT_CUSTOM,
     RANDOM_SEARCH_AGENT,
     AGENT_4o,
     AGENT_4o_MINI,
