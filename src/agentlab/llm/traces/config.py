a75eafff8eaa2ecea9a5764d4a01c61b597caa5a
RohitP2005 <rohitprivate13@gmail.com>
2025-02-10 19:32:18 +0530

    Inital template

---
+++ src/agentlab/llm/traces/config.py
@@ -0,0 +1,5 @@
+HF_USERNAME = "your_username"
+HF_INDEX_DATASET = "your_username/agent_traces_index"
+HF_TRACE_DATASET = "your_username/agent_traces_data"
+WHITELISTED_BENCHMARKS = ["benchmark1", "benchmark2"]
+
