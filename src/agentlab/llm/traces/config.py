bcf9988b3321d53ff50931dadea00ac333595a6d
RohitP2005 <rohitprivate13@gmail.com>
2025-03-01 02:38:13 +0530

    removing old implementations

--- src/agentlab/llm/traces/config.py
+++
@@ -1,5 +0,0 @@
-HF_USERNAME = "your_username"
-HF_INDEX_DATASET = "your_username/agent_traces_index"
-HF_TRACE_DATASET = "your_username/agent_traces_data"
-WHITELISTED_BENCHMARKS = ["benchmark1", "benchmark2"]
-
