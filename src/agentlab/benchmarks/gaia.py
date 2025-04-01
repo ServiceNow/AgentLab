0cfe4be08939c05769476b4fb1ad2ae7f3abf769
Oleh Shliazhko <oleh.shliazhko@servicenow.com>
2025-04-01 13:06:22 +0200

    trust remote code flag for loading gaia bench from the hf repo

--- src/agentlab/benchmarks/gaia.py
+++ src/agentlab/benchmarks/gaia.py
@@ -105,7 +105,9 @@ class GaiaBenchmark(AbstractBenchmark):
 
     def model_post_init(self, __context: Any) -> None:
         if not self.dataset:
-            self.dataset = datasets.load_dataset("gaia-benchmark/GAIA", "2023_all")
+            self.dataset = datasets.load_dataset(
+                "gaia-benchmark/GAIA", "2023_all", split=self.split, trust_remote_code=True
+            )
         self.env_args_list = []
         for i, task in enumerate(self.dataset[self.split]):
             if self.level != "all" and task["Level"] != self.level:
