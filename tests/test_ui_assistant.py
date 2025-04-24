b58f8bcdab8863fa5553271cb823196c2b71a3f7
Thibault LSDC <78021491+TLSDC@users.noreply.github.com>
2025-04-24 15:10:57 -0400

    Fixing test

--- tests/test_ui_assistant.py
+++ tests/test_ui_assistant.py
@@ -6,4 +6,4 @@ def test_make_exp_args():
     """Basic unit test to detect refactoring errors."""
     exp_args = make_exp_args(AGENT_4o)
 
-    assert exp_args.agent_args.flags.action.demo_mode == "default"
+    assert exp_args.agent_args.flags.action.action_set.demo_mode == "default"
