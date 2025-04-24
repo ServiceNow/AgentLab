9e85d8883fb34997286fe86530eb1985605359f5
Thibault LSDC <78021491+TLSDC@users.noreply.github.com>
2025-04-24 14:56:36 -0400

    Improved typing

--- src/agentlab/ui_assistant.py
+++ src/agentlab/ui_assistant.py
@@ -7,7 +7,7 @@ from agentlab.experiments.launch_exp import import_object
 from agentlab.experiments.loop import EnvArgs, ExpArgs
 
 
-def make_exp_args(agent_args: AgentArgs, start_url="https://www.google.com"):
+def make_exp_args(agent_args: GenericAgentArgs, start_url="https://www.google.com"):
     try:
         agent_args.flags.action.action_set.demo_mode = "default"
     except AttributeError:
