4b1c45138e9d3370038004c9ac859aadcd73c15c
Chao Wang <chao.wang@servicenow.com>
2025-06-09 18:11:56 +0000

    update

--- src/agentlab/agents/vl_agent/utils.py
+++ src/agentlab/agents/vl_agent/utils.py
@@ -8,11 +8,10 @@ import io
 import numpy as np
 
 
-def image_to_image_url(image: Union[Image.Image, np.ndarray]):
+def image_to_image_url(image: Union[Image.Image, np.ndarray]) -> str:
     if isinstance(image, np.ndarray):
         image = Image.fromarray(image)
-    if image.mode in ("RGBA", "LA"):
-        image = image.convert("RGB")
+    image = image.convert("RGB")
     buffer = io.BytesIO()
     image.save(buffer, format="JPEG")
     image_base64 = base64.b64encode(buffer.getvalue()).decode()
