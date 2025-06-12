d3a52d9746b908b7363868d06c945f5feaa2bad5
Patrice Bechard <patrice.bechard@servicenow.com>
2025-06-12 12:30:41 -0400

    remove deployment name from azure model args

--- src/agentlab/llm/llm_configs.py
+++ src/agentlab/llm/llm_configs.py
@@ -224,7 +224,6 @@ CHAT_MODEL_ARGS_DICT = {
     ### Azure
     "azure/gpt-4o-mini": AzureModelArgs(
         model_name="gpt-4o-mini",
-        # deployment_name="gpt-4o-mini-2024-07-18",
         max_total_tokens=128_000,
         max_input_tokens=128_000,
         max_new_tokens=16_384,
@@ -232,7 +231,6 @@ CHAT_MODEL_ARGS_DICT = {
     ),
     "azure/gpt-4o": AzureModelArgs(
         model_name="gpt-4o",
-        # deployment_name="gpt-4o-mini-2024-07-18",
         max_total_tokens=128_000,
         max_input_tokens=128_000,
         max_new_tokens=16_384,
@@ -240,7 +238,6 @@ CHAT_MODEL_ARGS_DICT = {
     ),
     "azure/gpt-4.1": AzureModelArgs(
         model_name="gpt-4.1",
-        # deployment_name="gpt-4.1",
         max_total_tokens=128_000,
         max_input_tokens=128_000,
         max_new_tokens=16_384,
@@ -248,7 +245,6 @@ CHAT_MODEL_ARGS_DICT = {
     ),
     "azure/gpt-4.1-mini": AzureModelArgs(
         model_name="gpt-4.1-mini",
-        # deployment_name="gpt-4.1-mini",
         max_total_tokens=128_000,
         max_input_tokens=128_000,
         max_new_tokens=16_384,
