024935e80225069a8ff1e42e6e70879b673b03da
Aman Jaiswal <66757799+amanjaiswal73892@users.noreply.github.com>
2025-07-04 19:57:07 -0400

    update run_osworld to use small test set and one task

--- experiments/run_osworld.py
+++ experiments/run_osworld.py
@@ -13,7 +13,7 @@ def main():
     n_jobs = 1
     os.environ["AGENTLAB_DEBUG"] = "1"
     study = make_study(
-        benchmark=OsworldBenchmark(),  # type: ignore
+        benchmark=OsworldBenchmark(test_set_name='test_small.json'),  # type: ignore
         agent_args=[OSWORLD_CLAUDE],
         comment="osworld debug 1",
         logging_level=logging.INFO,
@@ -21,7 +21,7 @@ def main():
     )
 
     if os.environ.get("AGENTLAB_DEBUG"):
-        study.exp_args_list = study.exp_args_list[:1]
+        study.exp_args_list = study.exp_args_list[-9:-8]
         study.run(n_jobs=1, n_relaunch=1, parallel_backend="sequential")
     else:
         study.run(n_jobs=n_jobs, n_relaunch=1, parallel_backend="ray")
