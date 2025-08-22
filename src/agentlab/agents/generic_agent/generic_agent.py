6161634c381b44d147aa88e33f2afca035bfa8e9
emilianopp <emilianopp550@gmail.com>
2025-08-22 22:42:08 +0000

    bandaid

--- src/agentlab/agents/generic_agent/generic_agent.py
+++ src/agentlab/agents/generic_agent/generic_agent.py
@@ -218,4 +218,3 @@ does not support vision. Disabling use_screenshot."""
         self.task = task
     def set_goal(self, goal):
         self.goal = goal
-        self.trajectory = self.privileged_observations.get_random(task=self.task, goal=self.goal)
