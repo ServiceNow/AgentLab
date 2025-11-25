323978d21a116e61e568fd80363ea45f8ad72183
Oleh Shliazhko <oleh.shliazhko@servicenow.com>
2025-11-25 13:49:41 +0000

    init files

---
+++ src/agentlab/backends/browser/__init__.py
@@ -0,0 +1,18 @@
+from agentlab.backends.browser.base import BrowserBackend, FunctionCall, ToolCallAction, ToolSpec
+from agentlab.backends.browser.env import BrowserEnv, BrowserEnvArgs
+from agentlab.backends.browser.mcp import MCPBrowserBackend, MCPClient
+from agentlab.backends.browser.mcp_playwright import MCPPlaywright
+from agentlab.backends.browser.playwright import AsyncPlaywright
+
+__all__ = [
+    "BrowserBackend",
+    "FunctionCall",
+    "ToolCallAction",
+    "ToolSpec",
+    "BrowserEnv",
+    "BrowserEnvArgs",
+    "MCPBrowserBackend",
+    "MCPClient",
+    "MCPPlaywright",
+    "AsyncPlaywright",
+]
