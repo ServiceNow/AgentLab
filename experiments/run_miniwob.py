212c0f410908bc6d46157959581a47aa64850532
Oleh Shliazhko <oleh.shliazhko@servicenow.com>
2025-11-26 16:38:49 +0000

    remove tape agent

--- experiments/run_miniwob.py
+++ experiments/run_miniwob.py
@@ -1,7 +1,6 @@
 import argparse
 import logging
 import os
-import sys
 
 from bgym import DEFAULT_BENCHMARKS
 from dotenv import load_dotenv
@@ -9,19 +8,16 @@ from dotenv import load_dotenv
 from agentlab.agents.generic_agent.agent_configs import GPT5_MINI_FLAGS
 from agentlab.agents.generic_agent.generic_agent import GenericAgentArgs
 from agentlab.agents.react_toolcall_agent import AgentConfig, LLMArgs, ReactToolCallAgentArgs
-from agentlab.agents.tapeagent.agent import TapeAgentArgs, load_config
 from agentlab.backends.browser.mcp_playwright import MCPPlaywright
 from agentlab.backends.browser.playwright import SyncPlaywright
 from agentlab.benchmarks.miniwob import MiniWobBenchmark
 from agentlab.experiments.study import make_study
-from agentlab.llm.chat_api import BaseModelArgs
 from agentlab.llm.llm_configs import CHAT_MODEL_ARGS_DICT
 
 fmt = "%(asctime)s - %(levelname)s - %(name)s:%(lineno)d - %(funcName)s() - %(message)s"
 logging.basicConfig(level=logging.INFO, force=True, format=fmt, handlers=[logging.StreamHandler()])
 logger = logging.getLogger(__name__)
 load_dotenv()
-
 
 
 def parse_args():
@@ -49,7 +45,6 @@ def parse_args():
 
 if __name__ == "__main__":
     args = parse_args()
-    config = load_config(args.config)
 
     if args.backend == "bgym":
         benchmark = DEFAULT_BENCHMARKS["miniwob"](n_repeats=1)
@@ -65,18 +60,17 @@ if __name__ == "__main__":
             chat_model_args=CHAT_MODEL_ARGS_DICT["azure/gpt-5-mini-2025-08-07"],
             flags=GPT5_MINI_FLAGS,
         )
-    elif args.agent == "react":
+    else:  # react
         agent_args = ReactToolCallAgentArgs(
-            llm_args=LLMArgs(model_name="azure/gpt-5-mini", temperature=1.0, max_total_tokens=128000),
+            llm_args=LLMArgs(
+                model_name="azure/gpt-5-mini", temperature=1.0, max_total_tokens=128000
+            ),
             config=AgentConfig(),
         )
-    else:
-        agent_args = TapeAgentArgs(agent_name=config.name, config=config)
 
     study = make_study(
         benchmark=benchmark,
         agent_args=agent_args,
-        comment=config.comment,
         logging_level=logging.INFO,
         logging_level_stdout=logging.INFO,
     )
@@ -84,4 +78,4 @@ if __name__ == "__main__":
         study.exp_args_list = study.exp_args_list[23:27]
         study.run(n_jobs=1, n_relaunch=1, parallel_backend="sequential")
     else:
-        study.run(n_jobs=config.n_jobs, n_relaunch=1, parallel_backend=config.parallel_backend)
+        study.run(n_jobs=8, n_relaunch=1, parallel_backend="ray")
